f6d5c236ca4642f1c6afb85929d92ee6533b70ce
MoranHorovitz <moran.horovitz@mail.huji.ac.il>
2026-05-01 11:09:34 +0300

    Add ReelsGuide route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import InnerCirclePage from "./pages/InnerCircle";
+import ReelsGuide from "./pages/ReelsGuide";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/" element={<InnerCirclePage />} />
+        <Route path="/guides/reels" element={<ReelsGuide />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
