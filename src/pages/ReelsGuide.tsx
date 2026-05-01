d2a8fc4084139be3c7e1fc3f085fbe6585b794c5
MoranHorovitz <moran.horovitz@mail.huji.ac.il>
2026-05-01 11:08:28 +0300

    Add ReelsGuide component with iframe

---
+++ src/pages/ReelsGuide.tsx
@@ -0,0 +1,9 @@
+export default function ReelsGuide() {
+  return (
+    <iframe
+      src="/guides/reels/index.html"
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+      title="מדריך רילז"
+    />
+  );
+}
